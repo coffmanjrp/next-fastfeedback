116017fa94ec8b1ff8e44fb56602c8cde6a94907
coffmanjrp <coffmanjrp@gmail.com>
2021-04-12 13:47:15 -0700

    Fix width for navbar and table

--- components/DashboardShell.jsx
+++ components/DashboardShell.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Flex, Link, Stack, Avatar, Button } from '@chakra-ui/react';
+import { Flex, Link, Stack, Avatar, Button, Box } from '@chakra-ui/react';
 import { LogoIcon } from '@/utils/customIcons';
 import { useAuth } from '@/lib/auth';
 
@@ -9,41 +9,41 @@ const DashboardShell = ({ children }) => {
 
   return (
     <Flex flexDirection="column">
-      <Flex
-        backgroundColor="white"
-        justifyContent="space-between"
-        alignItems="center"
-        py={4}
-        px={8}
-        w="full"
-        h="70px"
-        m="0 auto"
-        maxW="1250px"
-      >
-        <Stack spacing={4} isInline alignItems="center">
-          <NextLink href="/" passHref>
-            <Link>
-              <LogoIcon color="black" fontSize="24px" mt="-1px" />
-            </Link>
-          </NextLink>
-          <NextLink href="/dashboard" passHref>
-            <Link mr={4}>Sites</Link>
-          </NextLink>
-          <NextLink href="/feedback" passHref>
-            <Link>Feedback</Link>
-          </NextLink>
-        </Stack>
-        <Flex alignItems="center">
-          {user && (
-            <Button variant="ghost" mr={2} onClick={() => signout()}>
-              Log out
-            </Button>
-          )}
-          <Avatar size="sm" src={user && user.photoUrl} />
+      <Box backgroundColor="white" w="full" h="70px">
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          py={4}
+          px={8}
+          w="full"
+          m="0 auto"
+          maxW="1250px"
+        >
+          <Stack spacing={4} isInline alignItems="center">
+            <NextLink href="/" passHref>
+              <Link>
+                <LogoIcon color="black" fontSize="24px" mt="-1px" />
+              </Link>
+            </NextLink>
+            <NextLink href="/dashboard" passHref>
+              <Link mr={4}>Sites</Link>
+            </NextLink>
+            <NextLink href="/feedback" passHref>
+              <Link>Feedback</Link>
+            </NextLink>
+          </Stack>
+          <Flex alignItems="center">
+            {user && (
+              <Button variant="ghost" mr={2} onClick={() => signout()}>
+                Log out
+              </Button>
+            )}
+            <Avatar size="sm" src={user && user.photoUrl} />
+          </Flex>
         </Flex>
-      </Flex>
+      </Box>
       <Flex backgroundColor="gray.100" p={8} h="100vh">
-        <Flex maxWidth="980px" direction="column" width="100%" m="0 auto">
+        <Flex maxWidth="1150px" direction="column" w="full" m="0 auto">
           {children}
         </Flex>
       </Flex>
