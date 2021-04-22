b4f07df3c984897bc26472762bc6ee059c817179
coffmanjrp <coffmanjrp@gmail.com>
2021-04-21 17:40:50 -0700

    Get site data for feedback api

--- pages/api/feedback/[...site].js
+++ pages/api/feedback/[...site].js
@@ -1,11 +1,12 @@
-import { getAllFeedback } from '@/lib/db-admin';
+import { getAllFeedback, getSite } from '@/lib/db-admin';
 
 export default async (req, res) => {
   try {
     const [siteId, route] = req.query.site;
     const { feedback } = await getAllFeedback(siteId, route);
+    const { site } = await getSite(siteId);
 
-    res.status(200).json({ feedback });
+    res.status(200).json({ feedback, site });
   } catch (error) {
     if (error) {
       res.status(500).json({ error });
